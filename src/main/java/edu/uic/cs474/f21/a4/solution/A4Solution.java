9a7118a67e10b585c9ab066e2b9da6e775411899
makhariaaayush <63086244+makhariaaayush@users.noreply.github.com>
2021-11-12 22:31:40 -0600

    Test2 and Test 3 Done

--- src/main/java/edu/uic/cs474/f21/a4/solution/A4Solution.java
+++ src/main/java/edu/uic/cs474/f21/a4/solution/A4Solution.java
@@ -2,6 +2,8 @@ package edu.uic.cs474.f21.a4.solution;
 
 import edu.uic.cs474.f21.a4.*;
 
+import java.util.Optional;
+
 public class A4Solution extends Assignment4 {
     private Interpreter interpreter = new Interpreter();
     @Override
@@ -9,7 +11,7 @@ public class A4Solution extends Assignment4 {
         switch(c.getClass().getSimpleName()) {
             case "ClassDefExpression": {
                 ClassDefExpression def = (ClassDefExpression) c;
-                ClassValue theClassDefined = new ClassValue();
+                ClassValue theClassDefined = new ClassValue(def.superName,def.fields);
                 e = e.bind(def.className,theClassDefined);
                 return evaluate(def.body,e);
 
@@ -17,18 +19,28 @@ public class A4Solution extends Assignment4 {
             case "InstanceOfExpression": {
                 InstanceOfExpression ioe = (InstanceOfExpression) c;
                 Value val = evaluate(ioe.target,e);
+                Value t = new Value.BoolValue(true);
+                Value f = new Value.BoolValue(false);
                 if(val instanceof ObjectValue){
-                    ObjectValue obj = (ObjectValue) val;
-                    return new Value.BoolValue(true);
-                }else{
-                    return new Value.BoolValue(false);
+                    ObjectValue ioeobj = (ObjectValue) val;
+                    ClassValue ioeclassValue = (ClassValue) e.lookup(ioeobj.theName);
+                    if (!((ObjectValue) val).theName.equals(ioe.className)) {
+                        if (ioeclassValue.superClass.isEmpty() || !ioeclassValue.superClass.get().
+                                equals(ioe.className)) {
+                            return f;
+                        }
+                    }
+                    return  t;
                 }
-
+                return  f;
             }
+
+
             case "NewExpression": {
-                NewExpression newe =  (NewExpression) c;
-                ObjectValue newObject = new ObjectValue();
-                return newObject;
+                NewExpression newe = (NewExpression) c;
+                ClassValue neweclassValue = (ClassValue) e.lookup(newe.className);
+                ObjectValue neweObject = new ObjectValue(newe.className,neweclassValue.fields);
+                return neweObject;
 
             }
         }
@@ -36,9 +48,24 @@ public class A4Solution extends Assignment4 {
     }
 
     public static class ClassValue extends Value{
+        Optional<Name> superClass;
+        Field[] fields;
+
+        public ClassValue(Optional<Name> superClass, Field[] fields) {
+            this.superClass = superClass;
+            this.fields = fields;
 
     }
+    }
+
     public static class ObjectValue extends Value{
+        Name theName;
+        Field[] fields;
+
+        public ObjectValue(Name theName, Field[] fields) {
+            this.theName = theName;
+            this.fields = fields;
+        }
 
     }
 }
