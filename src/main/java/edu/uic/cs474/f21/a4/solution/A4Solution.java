eee7ed921f77e549e9b194689c8bac5b6bb1b14e
makhariaaayush <63086244+makhariaaayush@users.noreply.github.com>
2021-11-18 02:29:18 -0600

    All 10 Done

--- src/main/java/edu/uic/cs474/f21/a4/solution/A4Solution.java
+++ src/main/java/edu/uic/cs474/f21/a4/solution/A4Solution.java
@@ -2,6 +2,10 @@ package edu.uic.cs474.f21.a4.solution;
 
 import edu.uic.cs474.f21.a4.*;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class A4Solution extends Assignment4 {
@@ -9,10 +13,17 @@ public class A4Solution extends Assignment4 {
     @Override
     public Value evaluate(Expression c, Environment e) {
         switch(c.getClass().getSimpleName()) {
-//            3 4 7
             case "ClassDefExpression": {
                 ClassDefExpression def = (ClassDefExpression) c;
-                ClassValue theClassDefined = new ClassValue(def.superName, def.methods, def.fields);
+                ClassValue theClassDefined = new ClassValue();
+                theClassDefined.name = def.className;
+                if(def.superName.isPresent()){
+                    theClassDefined.parent = Optional.of((ClassValue) e.lookup(def.superName.get()));
+                }else{
+                    theClassDefined.parent = Optional.empty();
+                }
+                theClassDefined.fields = def.fields;
+                theClassDefined.methods = def.methods;
                 e = e.bind(def.className,theClassDefined);
                 return evaluate(def.body,e);
 
@@ -21,87 +32,106 @@ public class A4Solution extends Assignment4 {
 //                2 and 3
             case "InstanceOfExpression": {
                 InstanceOfExpression ioe = (InstanceOfExpression) c;
-                Value val = evaluate(ioe.target,e);
-                if(val instanceof ObjectValue){
-                    ObjectValue ioeobj = (ObjectValue) val;
-                    ClassValue ioeclassValue = (ClassValue) e.lookup(ioeobj.theName);
-                    if (!((ObjectValue) val).theName.equals(ioe.className)) {
-                        if (ioeclassValue.superClass.isEmpty() || !ioeclassValue.superClass.get().
-                                equals(ioe.className)) {
-                            return new Value.BoolValue(false);
-                        }
+                Value val = evaluate(ioe.target, e);
+                if (val instanceof ObjectValue) {
+                    ObjectValue obj = (ObjectValue) val;
+                    if (obj.klass.name.equals(ioe.className)) {
+                        return new Value.BoolValue(true);
                     }
-                    return  new Value.BoolValue(true);
+                    ClassValue clss = obj.klass;
+                    if (clss.parent.isPresent()) {
+                        ClassValue p = clss.parent.get();
+                        if (p.name.equals(ioe.className)){
+                            return new Value.BoolValue(true);
+                        }
+                        else
+                            clss = p;
+                    }
+                    return new Value.BoolValue(false);
+                } else {
+                    return new Value.BoolValue(false);
                 }
-                return  new Value.BoolValue(false);
             }
-//Test 3 same as 2 just that here b extends a so b instance of A is true
-//                2 and 4
+
             case "NewExpression": {
                 NewExpression newe = (NewExpression) c;
-                ClassValue neweclassValue = (ClassValue) e.lookup(newe.className);
-                ObjectValue neweObject = new ObjectValue(newe.className, neweclassValue.methods, neweclassValue.fields);
-                return neweObject;
+                ObjectValue newObject = new ObjectValue();
+                newObject.klass = (ClassValue) e.lookup(newe.className);
+                ClassValue klass = newObject.klass;
+                while (true) {
+                    for (Field f : klass.fields) {
+                        Value v = evaluate(f.initializer, e);
+                        newObject.fields.put(f.name, v);
+                    }
+                    if(klass.parent.isPresent()){
+                        klass = klass.parent.get();
+                    }
+                    else
+                        break;
+                }
+                        return newObject;
             }
 
             case "ReadFieldExpression": {
-//                4 6
-//                Test 4 we create a class for A with single field which is then read as value of A when passed as instance of an object
-//                Test 5 we need to read 2 fields and return the respective values
                 ReadFieldExpression rfe = (ReadFieldExpression) c;
                 ObjectValue rfeObject = (ObjectValue) evaluate(rfe.receiver, e);
-                for (Field field : rfeObject.fields)
-                    if (rfe.fieldName.theName.equals(field.name.theName)) {
-                        Value.IntValue value = (Value.IntValue) evaluate(field.initializer, e);
-                        return new Value.IntValue(value.val);
-                    }
+                return rfeObject.fields.get(rfe.fieldName);
             }
 //            test 6 we need to climb the hierarchy and get the inherited value
 
             case "CallMethodExpression": {
-//                7 we take the argument convert to receiver and then bind it into and expression and reurn the value
-                CallMethodExpression cme = (CallMethodExpression) c;
-                ObjectValue cmeObject = (ObjectValue) evaluate(cme.receiver,e);
-                Method[] methods = cmeObject.methods;
-                for (Method method : methods) {
-                    if (method.name.equals(cme.methodName)) {
-                        int count = 0;
-                        e = e.bind(new Name("this"), evaluate(cme.receiver, e));
-                        Expression[] arguments = cme.arguments;
-                        for (int i = 0; i < arguments.length; i++) {
-                            Expression ex = arguments[i];
-                            e = e.bind(method.arguments[count], evaluate(ex, e));
-                            count = count + 1;
-                        }
-                        return evaluate(method.body, e);
-                    }
+                CallMethodExpression  call = (CallMethodExpression) c;
+                Value[] actualValue = new Value[call.arguments.length];
+                for(int i = 0 ; i <  actualValue.length; i++)
+                {
+                    actualValue[i] = evaluate(call.arguments[i],e);
                 }
+                ObjectValue receiver = (ObjectValue) evaluate(call.receiver,e);
+                Method a;
+                ClassValue klass = receiver.klass;
+                while(true) {
+                     Optional<Method> maybea = Arrays.stream(klass.methods).filter(method -> method.name.equals(call.methodName)).findAny();
+                     if(maybea.isPresent()){
+                         a = maybea.get();
+                         break;
+                     }
+                     if(klass.parent.isPresent()){
+                         klass = klass.parent.get();
+                     }else{
+                         throw new Error("Method "+call.methodName + " does not exist in class "+ receiver.klass.name);
+                     }
+                }
+                Environment envThatKnows = e;
+                for(int i = 0 ;i < a.arguments.length;i++){
+                     envThatKnows = envThatKnows.bind(a.arguments[i],actualValue[i]);
+                }
+                envThatKnows = envThatKnows.bind((new Name("this")),receiver);
+                return evaluate(a.body,envThatKnows);
+
+            }
+            case "WriteFieldExpression" :{
+                WriteFieldExpression write = (WriteFieldExpression) c;
+                ObjectValue target = (ObjectValue) evaluate(write.receiver,e);
+
+                Value newVal = evaluate(write.newValue,e);
+                target.fields.put(write.fieldName,newVal);
+
+                return newVal;
             }
         }
         return interpreter.eval(c,e,null,this);
     }
 
     public static class ClassValue extends Value{
-        Optional<Name> superClass;
+        Name name;
         Field[] fields;
         Method[] methods;
-
-        public ClassValue(Optional<Name> superClass, Method[] methods, Field[] fields) {
-            this.superClass = superClass;
-            this.methods = methods;
-            this.fields = fields;
-        }
+        Optional<ClassValue> parent;
     }
 
     public static class ObjectValue extends Value{
-        Method[] methods;
-        Name theName;
-        Field[] fields;
+        ClassValue klass;
+        Map<Name,Value> fields = new HashMap<>();
 
-        public ObjectValue(Name theName, Method[] methods, Field[] fields) {
-            this.theName = theName;
-            this.fields = fields;
-            this.methods = methods;
-        }
     }
 }
